14d852ab74284e9fb1b9cda495bffe67ea2c0d3e
Elhanan Avihail <etsavihail@gmail.com>
2022-10-27 17:49:14 +0300

    lazy image

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,32 @@
-(async function redirectWhenSiteIsUp() {
-    const url = 'https://volunteery.onrender.com'
+// init()
+window.addEventListener('load', init)
+function init() {
+    console.log('init');
     
+    // redirectWhenSiteIsUp()
+    setTimeout(()=>{
+        console.log('Waiting...');
+        
+        document.body.classList.add('waiting')
+    },500)
+
+}
+
+
+async function redirectWhenSiteIsUp() {
+    const url = 'https://volunteery.onrender.com'
+
     try {
         await fetch(url)
     } catch (err) {
         console.log(err);
     }
-    
+
     console.log('Going to site');
     document.body.classList.add('complete')
-    setTimeout(()=> {
+    setTimeout(() => {
         window.location.replace(url)
     }, 1000)
-})()
+}
+
 
