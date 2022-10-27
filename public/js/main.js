a1dd7b4e702e31e8303809b8866735bf454d3f17
Elhanan Avihail <etsavihail@gmail.com>
2022-10-27 14:27:49 +0300

    complete class

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,16 @@
 (async function redirectWhenSiteIsUp() {
     const url = 'https://volunteery.onrender.com'
+    
     try {
         await fetch(url)
     } catch (err) {
         console.log(err);
     }
     
-    console.log('Go to site');
-    window.location.replace(url)
+    console.log('Going to site');
+    document.body.classList.add('complete')
+    setTimeout(()=> {
+        window.location.replace(url)
+    }, 1000)
 })()
+
